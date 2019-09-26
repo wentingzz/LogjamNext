0fd1a56cca820c78c999610c41e4f33a6b99de5f
jpschmid <jpschmid@ncsu.edu>
2019-09-26 11:13:55 -0400

    Add outline for unzip tests

---
+++ tests/test_unzip.py
@@ -0,0 +1,21 @@
+import unittest
+
+class RecursiveUnzipTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+        # Prepare compressed data for each test
+        # (zip, gz, tar.gz, 7z, tar) plus multiple layer versions
+
+    def test_targz():
+        # Call recursive unzip
+
+        # Verify all files unzipped
+        pass
+
+    def test_tar():
+        pass
+
+    def test_zip():
+        pass
