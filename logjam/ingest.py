39db43254e39ef4f4c359c91b208548a1a9abeba
Nathaniel Brooks <ngbrooks@ncsu.edu>
2019-10-01 09:25:09 -0400

    #47 Got Bug Fixed
    Ensured that only directories are considered and that DS_Store is ignored.
    
    Towards #47

--- logjam/ingest.py
+++ logjam/ingest.py
@@ -117,8 +117,7 @@ def main():
 def ingest_log_files(input_root, output_root, scratch_space):
     for entity in os.listdir(input_root):
         full_path = os.path.join(input_root,entity)
-        #if os.path.isdir(full_path):
-        if entity != ".DS_Store":
+        if os.path.isdir(full_path) and entity != ".DS_Store":
             searchAnInspectionDirectory(full_path, output_root, scratch_space)
         else:
             logging.debug("Ignored non-StorageGRID file: %s", full_path)
