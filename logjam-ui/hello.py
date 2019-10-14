6d35784cb6d3a1993b54e8b0c687642739f4641f
jpschmid <jpschmid@ncsu.edu>
2019-10-14 16:48:41 -0400

    Add pie chart example

--- logjam-ui/hello.py
+++ logjam-ui/hello.py
@@ -1,10 +1,10 @@
-from flask import (Flask, url_for, redirect)
+from flask import (Flask, render_template)
 
-app = Flask(
-        __name__,
-        static_folder="./static"
-)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect(url_for("static", filename="index.html"))
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="asdf", port=5601)
