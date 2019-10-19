1db217b057c21d0ec6959cdb99c1ef02d60c1a94
Nathaniel Brooks <ngbrooks@ncsu.edu>
2019-10-18 21:44:35 -0400

    #69 Fleshing Out & Adding Test Cases
    Added more functionality to TimePeriod, ScanRecord, and Scan classes.
    Made them work better with Python operators, such as str(), in, & ==.
    Added unit test file to test their functionality.
    
    Towards issue #69

--- src/logjam-ingest/incremental.py
+++ src/logjam-ingest/incremental.py
@@ -12,7 +12,7 @@ from collections import namedtuple
 class TimePeriod:
     """ Represents a period in time defined by a start and stop time. These start
     and stop times are measured in integer seconds since the 'epoch'. On Unix this
-    is 1970, 00:00:00 (UTC).
+    is 1970, 00:00:00 (UTC). Designed to be immutable.
     """
     
     def __init__(self, start, stop):
@@ -20,116 +20,197 @@ class TimePeriod:
         """
         assert start < stop, "Start time must be before stop time"
         
-        self.start = int(start)
-        self.stop = int(stop)
+        self._start = int(start)
+        self._stop = int(stop)
     
-    def contains(self, new_time):
-        """ Checks to see if the new time is within this time period. Start is
-        inclusive & stop is exclusive.
+    def __eq__(self, other):
+        """ Returns whether two TimePeriods are equal.
+        """
+        if not isinstance(other, TimePeriod):
+            return NotImplemented
+        
+        return self._start == other._start and self._stop == other._stop
+    
+    def __str__(self):
+        """ Returns a readable string representation of this object.
+        """
+        return ' '.join([str(self._start), str(self._stop)])
+    
+    def __contains__(self, new_time):
+        """ Checks if new time is in the period [start, stop).
         """
         new_time = int(new_time)
-        return start <= new_time and new_time < stop
+        return self._start <= new_time and new_time < self._stop
+    
+    @property
+    def start(self):
+        """ Getter property for start
+        """
+        return self._start
+    
+    @property
+    def stop(self):
+        """ Getter property for stop
+        """
+        return self._stop
+    
+    @classmethod
+    def ancient_history(cls):
+        """ Returns a timestamp in ancient history, which for the purposes of
+        this program is the date 1900-01-01
+        """
+        seconds_in_minute = 60
+        seconds_in_hour = 60 * seconds_in_minute
+        seconds_in_day = 24 * seconds_in_hour
+        seconds_in_year = 365 * seconds_in_day
+        return 0 - 70 * seconds_in_year             # 70 years before 'epoch' (Jan 1900)
 
 
-class Scan:
-    """
+class ScanRecord:
+    """ Simple record for storing the scan's time period (start + stop), input
+    directory, and last searched path. Designed to be immutable.
     """
     
-    def __init__(self,time_period=TimePeriod(0,0),input_dir="",last_path=""):
-        """ Construct ScanRecord with designated fields.
-        """
-        self.time_period = time_period
-        self.input_directory = input_dir
-        self.last_path = last_path
+    def __init__(self, line):
+        assert len(line) > 0, "Should not be an empty string"
+    
+        space_tokens = line.split(' ')
+        assert len(space_tokens) >= 4, "Should be at least 4 separate tokens"
+        
+        start = int(space_tokens[0])
+        stop = int(space_tokens[1])
+        self.time_period = TimePeriod(start, stop)
+        
+        quote_tokens = line.split('"')
+        assert len(quote_tokens) == 5, "Should be exactly 5 tokens upon split"
+        
+        self.input_dir = quote_tokens[1]
+        assert len(self.input_dir) > 0, "Input directory should be given"
+        
+        self.last_path = quote_tokens[3]
+    
+    def __str__(self):
+        time = str(self.time_period)
+        input = '"' + str(self.input_dir) + '"'
+        last = '"' + str(self.last_path) + '"'
+        return ' '.join([time, input, last])
     
     def is_complete(self):
         """ Checks to see if this record represents a complete scan. A complete
         scan is denoted by a scan with no last path.
         """
-        assert isintance(self.last_path, str), "Last path should be a string"
-        assert len(self.input_dir) > 0, "Input directory should be given"
-        
-        return len(last_path) == 0
+        return len(self.last_path) == 0
 
 
-ScanRecord = namedtuple("ScanRecord", ["time_period","input_directory","last_path"])
-""" Simple record for storing the scan's time period (start + stop), input
-directory, and last searched path.
-"""
-
-
-def compute_scan_period(history_file,stop_time=None):
-    """ Computes a new period for scanning based on the last scan. If the last
-    scan successfully completed, computes start as the stop of the last scan and stop
-    as 6 minutes before the current system time or a caller provided value. If the last
-    scan was not successful, justs uses the scanning period of the previous scan.
+class Scan:
+    """ Represents an active scan of the input directory.
     """
-    assert os.path.isabs(history_file), "File path should be absolute"
     
-    start_time = 0
-    
-    if os.stat(history_file).st_size != 0:              # history file is not empty
-        scan_record = read_last_scan(history_file)
+    def __init__(self,input_dir):
+        """ Constructs a Scan which operates on the given input directory.
+        """
+        assert os.path.exists(input_dir), "File path must exist"
+        assert os.path.isabs(input_dir), "File path should be absolute"
         
-        if not scan_record.is_complete():               # last scan didn't finish
-            start_time = scan_record.time_period.start
-            stop_time = scan_record.time_period.stop
-        else:                                           # last scan did finish
-            start_time = scan_record.time_period.stop
-            if stop_time == None:
-                stop_time  = int(time.time()) - 60 * 6; # 6 min before current time
+        self.safe_time = int(time.time()) - 6 * 60  # 6 minutes before current time
+        self.input_dir = input_dir                  # immutable after construction
+        
+        self.time_period = TimePeriod(TimePeriod.ancient_history(), self.safe_time)
+        self.last_path = None
     
-    else:                                               # history file was empty
-        start_time = -60 * 60 * 24 * 365 * 30           # 30 years before epoch
-        if stop_time == None:
-            stop_time  = int(time.time()) - 60 * 6;     # 6 min before current time
+    def update_from_history_file(self, history_file):
+        """
+        """
+        assert os.path.exists(history_file), "History file must exist"
+        
+        if os.stat(history_file).st_size == 0:      # no previous scans, keep defaults
+            return
     
-    assert stop_time <= int(time.time()), "Stop time cannot be in the future"
-    return TimePeriod(start_time, stop_time)
-
-
-def file_within_scan(path, time_period):
-    """ Checks to see if the file denoted by path would be considered for a scan
-    that spans the given time period.
-    """
-    assert os.path.isabs(path), "File path should be absolute"
+        self.update_from_scan_record(extract_last_scan_record(history_file))
+        
+    def update_from_scan_record(self, scan_record):
+        """ 
+        """
+        assert scan_record.input_dir == self.input_dir, "Input directories must match"
+        
+        if scan_record.is_complete():               # completed, new period = then -> safe
+            new_start = min(scan_record.time_period.stop, self.safe_time-1)
+            new_stop = self.safe_time
+            self.time_period = TimePeriod(new_start, new_stop)
+            self.last_path = None
+        else:                                       # didn't finish, adopt old period
+            self.time_period = scan_record.time_period
+            self.last_path = scan_record.last_path
+        
+        assert self.time_period.stop <= self.safe_time, "Must remain within safe time"
+        
+    def to_scan_record(self):
+        """
+        """
+        return
     
-    modification_time = os.path.getmtime(path)
-    return time_period.contains(modification_time)
+    def should_consider_file(path):
+        """ Checks to see if the file denoted by path would be considered for this
+        over the given time period.
+        """
+        assert os.path.isabs(path), "File path should be absolute"
+        
+        modification_time = os.path.getmtime(path)
+        return modification_time in self.time_period
 
 
-def read_last_scan(history_file):
+# # # def compute_scan_period(history_file,stop=None):
+    # # # """ Computes a new period for scanning based on the last scan. If the last
+    # # # scan successfully completed, computes start as the stop of the last scan and stop
+    # # # as 6 minutes before the current system time or a caller provided value. If the last
+    # # # scan was not successful, justs uses the scanning period of the previous scan.
+    # # # """
+    # # # assert os.path.isabs(history_file), "File path should be absolute"
+    
+    # # # start = 0
+    
+    # # # if os.stat(history_file).st_size != 0:              # history file is not empty
+        # # # scan_record = read_last_scan(history_file)
+        
+        # # # if not scan_record.is_complete():               # last scan didn't finish
+            # # # start = scan_record.time_period.start
+            # # # stop = scan_record.time_period.stop
+        # # # else:                                           # last scan did finish
+            # # # start = scan_record.time_period.stop
+            # # # if stop == None:
+                # # # stop  = int(time.time()) - 60 * 6;      # 6 min before current time
+    
+    # # # else:                                               # history file was empty
+        # # # start = -60 * 60 * 24 * 365 * 30                # 30 years before epoch
+        # # # if stop == None:
+            # # # stop  = int(time.time()) - 60 * 6;          # 6 min before current time
+    
+    # # # assert stop <= int(time.time()), "Stop time cannot be in the future"
+    # # # return TimePeriod(start, stop)
+
+
+def extract_last_scan_record(history_file):
     """ Reads the last successful scan information from the scan history
     file denoted by the path and returns the information in a ScanRecord.
     """
+    assert os.path.exists(history_file), "File path should exist"
     assert os.path.isabs(history_file), "File path should be absolute"
+    assert os.stat(history_file).st_size > 0, "File must contain at least one record"
     
-    with open(history_file, "r") as file:
-        last_line = file.readlines()[-1]
-        
-        tokens = last_line.split()
-        start = int(tokens[0])
-        stop = int(tokens[1])
-        input_dir = tokens[1:-2]
-        last_path = tokens[1:-2]
-        
-        return ScanRecord(TimePeriod(start,stop),input_dir,last_path)
+    with open(history_file, "r") as file_stream:
+        last_line = file_stream.readlines()[-1]
+        return ScanRecord(last_line)
 
 
-def write_last_scan(history_file, scan_record):
+def append_scan_record(history_file, scan_record):
     """ Writes successful scan information to the scan history file denoted by
     the path. The scan time period (start & stop), input directory, and last searched
     path are written as a single line to the file.
     """
     assert os.path.isabs(history_file), "File path should be absolute"
-    assert len(scan_record.input_directory) > 0, "Input directory should be given"
+    assert len(scan_record.input_dir) > 0, "Input directory should be given"
     
     with open(history_file, "a") as file:
-        s = str(scan_record.time_period.start) +
-            str(scan_record.time_period.stop) +
-            '"' + scan_record.input_directory + '"' +
-            '"' + scan_record.last_path + '"'
-            
-        file.write(s+"\n")
+        file.write(scan_record.to_str()+"\n")
     
     return
---
+++ src/logjam-ingest/test_incremental.py
@@ -0,0 +1,85 @@
+"""
+@author Nathaniel Brooks
+Tests the functionality from the incremental.py file.
+"""
+
+
+import unittest
+import os
+import time
+import stat
+import subprocess
+
+
+import incremental
+
+
+class TimePeriodTestCase(unittest.TestCase):
+    """ Tests the TimePeriod class for its basic functionality.
+    """
+    
+    def test_init(self):
+        tmp = incremental.TimePeriod(0,1)
+        self.assertEqual(0, tmp.start)
+        self.assertEqual(1, tmp.stop)
+        
+        self.assertRaises(AssertionError, incremental.TimePeriod, 1, 0)
+
+
+class ScanRecordTestCase(unittest.TestCase):
+    """ Tests the ScanRecord class for its basic functionality.
+    """
+    
+    def test_init(self):
+        tmp = incremental.ScanRecord('100200 200300 "/path/to/input" "./relative/path"')
+        self.assertEqual(incremental.TimePeriod(100200, 200300), tmp.time_period)
+        self.assertEqual(100200, tmp.time_period.start)
+        self.assertEqual(200300, tmp.time_period.stop)
+        self.assertEqual("/path/to/input", tmp.input_dir)
+        self.assertEqual("./relative/path", tmp.last_path)
+        
+        tmp = incremental.ScanRecord('1 2 "/path w/ spaces" "log.txt"')
+        self.assertEqual(incremental.TimePeriod(1, 2), tmp.time_period)
+        self.assertEqual(1, tmp.time_period.start)
+        self.assertEqual(2, tmp.time_period.stop)
+        self.assertEqual("/path w/ spaces", tmp.input_dir)
+        self.assertEqual("log.txt", tmp.last_path)
+        
+        tmp = incremental.ScanRecord('0 1 "/nfs" ""')
+        self.assertEqual(incremental.TimePeriod(0, 1), tmp.time_period)
+        self.assertEqual(0, tmp.time_period.start)
+        self.assertEqual(1, tmp.time_period.stop)
+        self.assertEqual("/nfs", tmp.input_dir)
+        self.assertEqual("", tmp.last_path)
+        
+        self.assertRaises(AssertionError, incremental.ScanRecord, '1 0 "" ""')
+        self.assertRaises(AssertionError, incremental.ScanRecord, '01""""')
+        self.assertRaises(AssertionError, incremental.ScanRecord, '0 1""""')
+        self.assertRaises(AssertionError, incremental.ScanRecord, '0 1 """"')
+        self.assertRaises(AssertionError, incremental.ScanRecord, '0 1 /path file.txt')
+    
+    def test_str(self):
+        tmp = incremental.ScanRecord('5 7 "/nfs" "./2001938907/log.txt"')
+        self.assertEqual('5 7 "/nfs" "./2001938907/log.txt"', str(tmp))
+        
+        tmp = incremental.ScanRecord('5 7 "/path w/ spaces" "log.txt"')
+        self.assertEqual('5 7 "/path w/ spaces" "log.txt"', str(tmp))
+    
+    def test_is_complete(self):
+        tmp = incremental.ScanRecord('0 1 "/nfs" "./folder/log.txt"')
+        self.assertFalse(tmp.is_complete())
+        
+        tmp = incremental.ScanRecord('0 1 "/nfs" ""')
+        self.assertTrue(tmp.is_complete())
+
+
+
+
+class ScanTestCase(unittest.TestCase):
+    """ Tests the business logic of the Scan class.
+    """
+    
+    def test_init(self):
+        return
+    
+    
