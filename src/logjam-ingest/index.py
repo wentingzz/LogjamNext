5a410a5faf495d7fb430725ac984829ce799d018
jpschmid <jpschmid@ncsu.edu>
2019-10-30 14:34:35 -0400

    Add mappings file, create index on startup

--- src/logjam-ingest/index.py
+++ src/logjam-ingest/index.py
@@ -11,6 +11,8 @@ import elasticsearch
 from elasticsearch import Elasticsearch, helpers
 
 
+INDEX_NAME = "logjam"
+
 # Valid extensions to ingest
 validExtensions = [".txt", ".log"]
 # Valid extentionless files used in special cases
@@ -40,8 +42,8 @@ def stash_node_in_elk(fullPath, caseNum, categDirRoot, is_owned, es = None):
     platform = get_platform(None)
     if not os.path.exists(categDirRoot):
         os.makedirs(categDirRoot)
-    timestamp = "%.20f" % time.time()
-    basename = "-".join([caseNum, nodeName, timespan, storageGridVersion, timestamp])
+    timestamp = int(round(time.time() * 1000))  # Epoch milliseconds
+    basename = "-".join([caseNum, nodeName, timespan, storageGridVersion, str(timestamp)])
     node_dir = os.path.join(categDirRoot, basename)
     if not os.path.exists(node_dir):
         os.makedirs(node_dir)
@@ -58,7 +60,7 @@ def stash_node_in_elk(fullPath, caseNum, categDirRoot, is_owned, es = None):
     }
     if es:
         try:
-            es.index(index='logjam', doc_type='_doc', body = fields, id=fullPath)
+            es.index(index=INDEX_NAME, doc_type='_doc', body = fields, id=fullPath)
             logging.debug("Indexed %s to Elasticsearch", fullPath)
         except elasticsearch.exceptions.ConnectionError:
             logging.warn("Connection error sending doc %s to elastic search (file too big?)", fullPath)
@@ -154,8 +156,8 @@ def stash_file_in_elk(fullPath, filenameAndExtension, caseNum, categDirRoot, is_
             logging.critical("Unable to copy file: %s", e)
             raise e
 
-    timestamp = "%.20f" % time.time()
-    basename = "-".join([filenameAndExtension, timestamp])
+    timestamp = int(round(time.time() * 1000))
+    basename = "-".join([filenameAndExtension, str(timestamp)])
     categDirPathWithTimestamp = os.path.join(categDirRoot, caseNum, basename)
 
     fields = {
@@ -169,7 +171,7 @@ def stash_file_in_elk(fullPath, filenameAndExtension, caseNum, categDirRoot, is_
     }
     if es:
         try:
-            es.index(index='logjam', doc_type='_doc', body = fields, id=fullPath)
+            es.index(index=INDEX_NAME, doc_type='_doc', body = fields, id=fullPath)
             logging.debug("Indexed %s to Elasticsearch", fullPath)
         except elasticsearch.exceptions.ConnectionError:
             logging.warn("Connection error sending doc %s to elastic search (file too big?)", fullPath)
@@ -225,7 +227,7 @@ def is_storagegrid(fullpath, path):
                 if "bycast" in line:
                     return True
     except Exception as e:
-        logging.warning(str(e))
+        logging.warning("Error reading file to determine relevance: %s", str(e))
     return False
 #         open(path, 'r').read().find('bycast')
 
