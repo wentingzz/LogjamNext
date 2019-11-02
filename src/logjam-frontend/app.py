f7db6721e0416b181733ff5448a067464af6d3d5
Daniel Grist <dgrist@sd-vm24.csc.ncsu.edu>
2019-11-02 18:27:49 -0400

    Changed elasticsearch to include aggregation

--- src/logjam-frontend/app.py
+++ src/logjam-frontend/app.py
@@ -1,30 +1,25 @@
-from flask import (Flask, request, render_template, jsonify)
+from flask import (Flask, request, render_template, jsonify, json)
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-res=es.search(index='logjam', body={"query": { "bool": { "must": [
-    {"match": {"message": "Mar 19 11:25:51 vhacllimmgwp01 ADE: |20502835 32126 612199 HSTR %DED 2014-03-19T11:25:51.030415| NOTICE   0367 HSTR: Finished transferring successfully: bytesTransferred 430"}},
-                                                                        #{"match": {"version": "version"}},
-                                                                        #{"match": {"platform": "platform"}},
-                                                                      ]}}})
 
-
-count = 0
-
-for hit in res['hits']['hits']:
-    if hit['_score'] > 10:
-        print(hit['_score'])
-        count = count + 1
-
-
-resc=es.count(index='logjam', body={"query": { "bool": { "must": [
+test = 0
+r=es.search(index='logjam', _source="false", body={"min_score": 10.0,
+    "aggs": {"cases": {"terms": {"field": "node_name.keyword"}}},
+    "query": {"bool": {"should": 
         {"match": {"message": "Mar 19 11:25:51 vhacllimmgwp01 ADE: |20502835 32126 612199 HSTR %DED 2014-03-19T11:25:51.030415| NOTICE   0367 HSTR: Finished transferring successfully: bytesTransferred 430"}},
- ]}}})
+        }}})
 
-print(count)
-print(resc)
+for re in r['aggregations']['cases']['buckets']:
+    if re['key'] == "unknown":
+        test += re['doc_count']
+    else:
+        test += 1
+
+#print(json.dumps(r))
+print(test)
 
 
 #@app.route("/")
@@ -66,14 +61,58 @@ def get_query():
     
     version = request.json.get('storagegrid_version')
 
-    res=es.count(index='logjam', body={"query": { "bool": { "must": [
-                                                                            {"match": {"message": message}},
-                                                                            {"match": {"storagegrid_version": version}},
-                                                                            {"match": {"platform": platform}},
-                                                                            ]}}})
-   
-    print(res)
-    print(count)
+    total_hits = 0
+    total_no_hits = 0
+
+    if (version): 
+        total_hits_q=es.search(index='logjam', _source="false", body={"min_score": "10.0", "aggs": {"cases": { "terms": { "field": "node_name"}}},
+            "query": { "bool": { "should": [
+                {"match": {"message": message}},
+                {"match": {"storagegrid_version": version}},
+                #{"match": {"platform": platform}},
+            ]}}})
+
+        for  hits in total_hits_q['aggregations']['cases']['buckets']:
+            if hits['key'] == "unknown":
+                total_hits += hits['doc_count']
+            else:
+                total_hits += 1
+        
+
+        total_no_hits_q=es.search(index='logjam', _source="false", body={"max_score": "10.0", "aggs": {"cases": { "terms": { "field": "node_name"}}},
+            "query": { "bool": { "should": [
+                {"match": {"message": message}},
+                {"match": {"storagegrid_version": version}},
+                #{"match": {"platform": platform}},
+            ]}}})
+
+        for  hits in total_no_hits_q['aggregations']['cases']['buckets']:                                                                                                               if hits['key'] == "unknown":                                                                                                                                                    total_no_hits += hits['doc_count']                                                                                                                                      else:                                                                                                                                                                           total_no_hits += 1   
+
+    else:
+        total_hits_q=es.search(index='logjam', _source="false", body={"min_score": "10.0", "aggs": {"cases": { "terms": { "field": "node_name"}}},
+            "query": { "bool": { "should": [
+                {"match": {"message": message}},
+                #{"match": {"platform": platform}},
+            ]}}})
+            
+        for  hits in total_hits_q['aggregations']['cases']['buckets']:                                                                                                                  if hits['key'] == "unknown":                                                                                                                                                    total_hits += hits['doc_count']                                                                                                                                         else:                                                                                                                                                                           total_hits += 1   
+                
+        total_no_hits_q=es.search(index='logjam', _source="false", body={"max_score": "10.0", "aggs": {"cases": { "terms": { "field": "node_name"}}},
+            "query": { "bool": { "should": [
+                {"match": {"message": message}},
+                #{"match": {"platform": platform}},
+            ]}}})
+
+        for  hits in total_no_hits_q['aggregations']['cases']['buckets']:                                                                                                               if hits['key'] == "unknown":                                                                                                                                                    total_no_hits += hits['doc_count']                                                                                                                                      else:                                                                                                                                                                           total_no_hits += 1   
+
+    
+    return jsonify([
+        {
+            "title":"Occurances",
+            "labels": ["Occurs", "Does not occur"],
+            "values": [total_hits, total_no_hits]
+        },
+        ])
 
 #if __name__ == "__main__":
 #    app.run(host="sd-vm24.csc.ncsu.edu", port=80)
