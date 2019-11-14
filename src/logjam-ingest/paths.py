9ac4b8d4a5bd5f5dcae1c90dd70d4b6f3cb65679
Nathaniel Brooks <ngbrooks@ncsu.edu>
2019-11-14 02:13:13 -0500

    #53 Fixed Small Bug with Source Correction

--- src/logjam-ingest/paths.py
+++ src/logjam-ingest/paths.py
@@ -33,7 +33,7 @@ class Entry:
         assert not relative.startswith("../"), "Leading .. not supported : "+relative
         assert not relative == "..", "Leading .. not supported : "+relative
         
-        source = source[:-1] if source.endswith('/') else source
+        source = source[:-1] if source.endswith("/") and source != "/" else source
         self._source = source                       # removed trailing /, format = ...n/n
         
         relative = relative[:-1] if relative.endswith('/') else relative
