9b08dd16e46ca3b06c0f755985b12f26f70d8782
Wenting <wenttzzheng@gmail.com>
2019-11-16 19:06:57 -0500

    #157 release the lock after manager scan has updated the scan history.

--- src/logjam-ingest/ingest.py
+++ src/logjam-ingest/ingest.py
@@ -28,6 +28,8 @@ import sqlite3
 import sys
 import time
 import signal
+import concurrent.futures
+import multiprocessing
 
 from elasticsearch import Elasticsearch, helpers
 from conans import tools
@@ -41,6 +43,7 @@ import fields
 
 code_src_dir = os.path.dirname(os.path.realpath(__file__))          # remove eventually
 intermediate_dir = os.path.join(code_src_dir, "..", "..", "data")   # remove eventually
+MAX_WORKERS = 8
 
 mappings_path = os.path.join(code_src_dir, "..", "elasticsearch/mappings.json")
 
@@ -102,7 +105,7 @@ def main():
     es_logger.setLevel(logging.WARNING)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.CRITICAL)
-    es = Elasticsearch([es_host], verify_certs = True)
+    es = Elasticsearch([es_host], verify_certs = True, maxsize = MAX_WORKERS)
     if not es.ping():
         logging.critical("Unable to connect to Elasticsearch")
         es = None
@@ -148,20 +151,26 @@ def ingest_log_files(input_dir, scratch_dir, history_dir, es_obj = None):
     scan = incremental.Scan(input_dir, history_dir, scratch_dir)
     
     entities = sorted(os.listdir(input_dir))
-    for e in range(len(entities)):
-        if e+1 != len(entities) and os.path.join(input_dir, entities[e+1]) < scan.last_path:
-            continue                                    # skip, haven't reached last_path
-        
-        entity = entities[e]
-        full_path = os.path.join(input_dir,entity)
-        if os.path.isdir(full_path):
-            case_num = fields.get_case_number(entity)
-            if case_num != None:
-                search_case_directory(scan, full_path, es_obj, case_num)
+    lock = multiprocessing.Manager().Lock()
+    
+    with concurrent.futures.ProcessPoolExecutor(max_workers = MAX_WORKERS) as executor:
+        for e in range(len(entities)):
+            if e+1 != len(entities) and os.path.join(input_dir, entities[e+1]) < scan.last_path:
+                continue                                    # skip, haven't reached last_path
+
+            entity = entities[e]
+            full_path = os.path.join(input_dir,entity)
+            if os.path.isdir(full_path):
+                case_num = fields.get_case_number(entity)
+                if case_num != None:
+                    f = executor.submit(search_case_directory, scan, full_path, es_obj, case_num, lock)
+                    f.result()
+                else:
+                    logging.debug("Ignored non-StorageGRID directory: %s", full_path)
             else:
-                logging.debug("Ignored non-StorageGRID directory: %s", full_path)
-        else:
-            logging.debug("Ignored non-StorageGRID file: %s", full_path)
+                logging.debug("Ignored non-StorageGRID file: %s", full_path)
+        
+    
     
     if graceful_abort:
         scan.premature_exit()
@@ -171,14 +180,26 @@ def ingest_log_files(input_dir, scratch_dir, history_dir, es_obj = None):
     return
 
 
-def search_case_directory(scan_obj, search_dir, es_obj, case_num):
+def search_case_directory(scan_obj, search_dir, es_obj, case_num, lock):
     """
     Searches the specified case directory for StorageGRID log files which have not
     been indexed by the Logjam system. Uses the Scan object's time period window to
     determine if a file has been previously indexed. Upon finding valid files, will
     send them to a running Elastissearch service via the Elastisearch object `es_obj`.
     """
-    return recursive_search(scan_obj, search_dir, es_obj, case_num)
+    child_scan = incremental.Scan(search_dir, scan_obj.history_dir, scan_obj.scratch_dir, str(case_num) + ".txt", str(case_num) + "-log.txt", scan_obj.safe_time)
+    recursive_search(child_scan, search_dir, es_obj, case_num)
+    if graceful_abort:
+        child_scan.premature_exit()
+    else:
+        child_scan.complete_scan()
+        lock.acquire()
+        scan_obj.just_scanned_this_path(search_dir)
+        lock.release()
+        unzip.delete_file(child_scan.history_log_file)
+        unzip.delete_file(child_scan.history_active_file)
+        
+    return
 
 
 def recursive_search(scan, start, es, case_num, depth=None, scan_dir=None):
